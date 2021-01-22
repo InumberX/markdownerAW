f36bf38d9f2a9f1ff6245b424fbf11d8cde509c7
InumberX <inumberx@gmaile.com>
2021-01-22 13:35:13 +0900

    Update _document.tsx

--- src/next/mk-downer/pages/_document.tsx
+++ src/next/mk-downer/pages/_document.tsx
@@ -73,7 +73,9 @@ class Document extends NextDocument {
            window.dataLayer = window.dataLayer || [];
            function gtag(){dataLayer.push(arguments);}
            gtag('js', new Date());
-           gtag('config', '${GA_TRACKING_ID}');
+           gtag('config', '${GA_TRACKING_ID}', {
+             page_path: window.location.pathname,
+           });
          `,
       }}
       key="gaScript"
